4038c38a5eacdd30d5b22a18e8ccc5bba4a8e9d4
G_H_Nuto <gustavonuto5@gmail.com>
2026-09-12 20:40:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -9,4 +10,4 @@ def read_root():
 
 @app.get("/testet1")
 def funcaoteste():
-    return {"teste": "deucerto"}
+    return {"teste": True, "num_aleatorio":random.randint(1,1000)}
