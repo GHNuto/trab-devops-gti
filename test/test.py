8cf9f75fac659b2fd890c4d5853258a5953ee04c
G_H_Nuto <gustavonuto5@gmail.com>
2026-09-20 21:53:53 -0300

    maiserros

--- test/test.py
+++ test/test.py
@@ -3,25 +3,32 @@ from unittest.mock import patch
 
 
 def test_root():
-    assert read_root() == {"ola": "mundo"}
-
+    result = read_root()
+    yield
+    assert result == {"ola": "mundo"}
 
 
 def test_funcaoteste():
-    with patch('rando.randint', return_value=12345):
+    with patch('src.main.random.randint', return_value=12345):
         result = funcaoteste()
+        yield
     assert result == {"teste": True, "num_aleatorio": 12345}
+
 
 def test_jogar_dado():
     with patch('src.main.random.randint', return_value=4):
         result = jogar_dado()
+        yield
     assert result == {"resultado_dado": 4}
+
 
 def test_saudacao_personalizada():
     result = saudacao_personalizada("Gustavo")
+    yield
     assert result == {"mensagem": "Olá, Gustavo! Seja bem-vindo à API."}
+
 
 def test_checar_status():
     result = checar_status()
+    yield
     assert result == {"status": "online", "servidor": "funcionando perfeitamente"}
-
