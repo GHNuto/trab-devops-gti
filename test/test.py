f5a7678eda89ccf77e2d1973c69accb57b53555b
G_H_Nuto <gustavonuto5@gmail.com>
2026-09-20 22:13:54 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,39 +1,37 @@
+import pytest
 from src.main import *
 from unittest.mock import patch
+import asyncio
 
 
-def test_root():
-    import asyncio
-    result = asyncio.run(read_root())
+@pytest.fixture
+def run_async_test():
+    
     yield
+
+
+def test_root(run_async_test):
+    result = asyncio.run(read_root())
     assert result == {"ola": "mundo"}
 
 
-def test_funcaoteste():
-    import asyncio
+def test_funcaoteste(run_async_test):
     with patch('src.main.random.randint', return_value=12345):
         result = asyncio.run(funcaoteste())
-        yield
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 
-def test_jogar_dado():
-    import asyncio
+def test_jogar_dado(run_async_test):
     with patch('src.main.random.randint', return_value=4):
         result = asyncio.run(jogar_dado())
-        yield
     assert result == {"resultado_dado": 4}
 
 
-def test_saudacao_personalizada():
-    import asyncio
+def test_saudacao_personalizada(run_async_test):
     result = asyncio.run(saudacao_personalizada("Gustavo"))
-    yield
     assert result == {"mensagem": "Olá, Gustavo! Seja bem-vindo à API."}
 
 
-def test_checar_status():
-    import asyncio
+def test_checar_status(run_async_test):
     result = asyncio.run(checar_status())
-    yield
     assert result == {"status": "online", "servidor": "funcionando perfeitamente"}
