9a64b435f017781809fc4a4513d1a658f7af6503
G_H_Nuto <gustavonuto5@gmail.com>
2026-09-20 22:09:53 -0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -3,32 +3,37 @@ from unittest.mock import patch
 
 
 def test_root():
-    result = read_root()
+    import asyncio
+    result = asyncio.run(read_root())
     yield
     assert result == {"ola": "mundo"}
-#so pra ativar a pr
+
 
 def test_funcaoteste():
+    import asyncio
     with patch('src.main.random.randint', return_value=12345):
-        result = funcaoteste()
+        result = asyncio.run(funcaoteste())
         yield
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 
 def test_jogar_dado():
+    import asyncio
     with patch('src.main.random.randint', return_value=4):
-        result = jogar_dado()
+        result = asyncio.run(jogar_dado())
         yield
     assert result == {"resultado_dado": 4}
 
 
 def test_saudacao_personalizada():
-    result = saudacao_personalizada("Gustavo")
+    import asyncio
+    result = asyncio.run(saudacao_personalizada("Gustavo"))
     yield
     assert result == {"mensagem": "Olá, Gustavo! Seja bem-vindo à API."}
 
 
 def test_checar_status():
-    result = checar_status()
+    import asyncio
+    result = asyncio.run(checar_status())
     yield
     assert result == {"status": "online", "servidor": "funcionando perfeitamente"}
