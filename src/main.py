39cb60451d85d03951fdc5104676dbf161145817
G_H_Nuto <gustavonuto5@gmail.com>
2026-09-20 21:41:13 -0300

    correcaodeerros

--- src/main.py
+++ src/main.py
@@ -5,21 +5,21 @@ app = FastAPI()
 
 
 @app.get("/olamundo")
-def read_root():
+async def read_root():
     return {"ola": "mundo"}
 
 @app.get("/numerorandom")
-def funcaoteste():
+async def funcaoteste():
     return {"teste": True, "num_aleatorio":random.randint(1,1000)}
 
 @app.get("/dado")
-def jogar_dado():
+async def jogar_dado():
     return {"resultado_dado": random.randint(1, 6)}
 
 @app.get("/saudacao/{nome}")
-def saudacao_personalizada(nome: str):
+async def saudacao_personalizada(nome: str):
     return {"mensagem": f"Olá, {nome}! Seja bem-vindo à API."}
 
 @app.get("/status")
-def checar_status():
+async def checar_status():
     return {"status": "online", "servidor": "funcionando perfeitamente"}
